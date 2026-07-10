945fafbe4fec2a4b063e5e543d7ee60d13cdfe3c
Johannes von Geisau <johannes.von.geisau@t-online.de>
2026-07-10 09:51:46 +0200

    Add additional named entity type: CLASS

--- src/main/java/edu/kit/kastel/mcse/ardoco/naer/model/NamedEntityType.java
+++ src/main/java/edu/kit/kastel/mcse/ardoco/naer/model/NamedEntityType.java
@@ -5,6 +5,6 @@ package edu.kit.kastel.mcse.ardoco.naer.model;
  * Represents the type of a {@link NamedEntity}.
  */
 public enum NamedEntityType {
-    COMPONENT, INTERFACE;
+    COMPONENT, INTERFACE, CLASS;
     // additional types can be added here...
 }
