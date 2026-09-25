bae11260d2de81a111b3f1ef6f55a24c6f7ba2bc
Johannes von Geisau <johannes.von.geisau@t-online.de>
2026-09-25 09:18:39 +0200

    add additional named entity types: DATAFILE, PROGRAMMING_LANGUAGE, ARCHITECTURAL_PATTERN

--- src/main/java/edu/kit/kastel/mcse/ardoco/naer/model/NamedEntityType.java
+++ src/main/java/edu/kit/kastel/mcse/ardoco/naer/model/NamedEntityType.java
@@ -5,6 +5,6 @@ package edu.kit.kastel.mcse.ardoco.naer.model;
  * Represents the type of a {@link NamedEntity}.
  */
 public enum NamedEntityType {
-    COMPONENT, INTERFACE, CLASS;
+    COMPONENT, INTERFACE, CLASS, DATAFILE, PROGRAMMING_LANGUAGE, ARCHITECTURAL_PATTERN;
     // additional types can be added here...
 }
